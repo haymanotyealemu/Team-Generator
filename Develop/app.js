169bba16701289786958bf178f24c2c8ffe18941
Haymanot W <haymanotalemu21@gmail.com>
2020-07-31 14:27:04 -0400

    prompting the user using inquirer module

---
+++ Develop/app.js
@@ -0,0 +1,121 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+// loading our classes 
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+// The Employee team data will be pushed in this array
+const teamMembers = [];
+
+let manager;
+let teamName;
+// Here we prompt the user basic info about the team
+
+function managerData() {
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"What is the name of your team ?",
+            name:"teamName"
+        },
+        {
+            type:"input",
+            message:"Who is the manager of this team ?",
+            name:"managerName"
+        },
+        {
+            type:"input",
+            message:"Enter the manager's id ?",
+            name:"managerId"
+        },
+        {
+            type:"input",
+            message:"Enter the manager's email ?",
+            name:"managerEmail"
+        },
+        {
+            type:"input",
+            message:"Enter the manager's email ?",
+            name:"managerEmail"
+        },
+        {
+            type:"input",
+            message:"Enter the manager's office number ?",
+            name:"officeNumber"
+        }
+    ]).then(managerAnswers => {
+        manager = new Manager(managerAnswers.managerName, managerAnswers.managerId, managerAnswers.managerEmail, managerAnswers.officeNumber);
+        teamName = managerAnswers.teamName;
+        console.log("Please enter the information about your team members");
+
+    });
+}
+
+// Now we ask the user(manager) about other team members basic information.
+function employeeData() {
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"Choose the employee's role",
+            name:"employeeRole",
+            choices:["Engineer", "Intern"]
+        },
+        // The following questions are derived based on the above employee Role
+        {
+            type:"input",
+            message:"What is the name of the employee?",
+            name:"employeeName"
+        },
+        {
+            type:"input",
+            message:"Enter the employee's ID",
+            name:"employeeId"
+        },
+        {
+            type:"input",
+            message:"Enter the employee's email",
+            name:"employeeEmail"
+        },
+        {
+            type:"input",
+            message:"What is the Engineer's Github?",
+            name:"github",
+            when:(userinput) => userinput.empployeeRole === "Engineer"
+        },
+        {
+            type:"input",
+            message:"What is the Intern's school?",
+            name:"school",
+            when:(userinput) => userinput.empployeeRole === "Intern"
+        },
+        {
+            type:"confirm",
+            message:"Would You like to add more employee to your team?",
+            name:"newEmployee"
+        }
+
+    ]).then(answers => {
+        // Here we create a new employee object using classes (Engineer and Intern) and push the to the team member array
+        if(answers.employeeRole === "Engineer"){
+            const employee = new Engineer(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.github);
+            teamMembers.push(employee);
+        }
+        else if(answers.employeeRole === "Intern"){
+            const employee2 = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.school);
+            teamMembers.push(employee2);
+        }
+        // Here we will call back the employeeData() if the user wants to add more employee to the team otherwise we render the HTML.
+        if(answers.newEmployee === true){
+            employeeData();
+        }
+        // Render HTML
+        else{
+
+        }
+
+
+
+    })
+}
+
