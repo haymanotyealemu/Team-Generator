af01b31acfaaadd1095a244239e9f961259df3b1
Haymanot W <haymanotalemu21@gmail.com>
2020-07-31 07:15:57 -0400

    create, define and export our classes

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,24 @@
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        // console.log(`Employee Name is ${this.name}`);
+        return (this.name);
+    }
+    getId() {
+        // console.log(`This employee has an id of ${this.id}`);
+        return (this.id);
+    }
+    getEmail() {
+        // console.log(`Employee email: ${this.email}`);
+        return (this.email);
+    }
+    getRole(){
+        // cpnsole.log(`Employee Role: ${this.getRole}`);
+        return "Employee";
+    }
+}
+module.exports = Employee;
